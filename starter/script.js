3b0e2c22e735cd4bfdc96d9a8ba5511e27adf393
yurisokolovicz <yurisokolovicz@gmail.com>
2023-01-15 17:42:58 -0300

    commit domingo

--- starter/script.js
+++ starter/script.js
@@ -317,7 +317,66 @@ if (favourite === 23) { // 23 === 23
 
 if (favourite !== 23) console.log('Why not 23?');
 // !== diferente.
+
+## Boolean Logic: AND, OR & NOT operators ##
+AND (&&): True when all are true. (A && B)
+OR: (||) true when one is true. (A || B)
+NOT: (!) inverts true and false values. (!A)
+
+const hasDriversLicense = true; //A
+const hasGoodVision = true //B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+if (shouldDrive) {
+    console.log('Sarah is able to drive!')
+} else {
+    console.log('Someone else should drive.')
+}
+
+const hasDriversLicense = true; //A
+const hasGoodVision = true //B
+const isTired = true; // C
+
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive!')
+} else {
+    console.log('Someone else should drive.')
+}
 */
 
+// ## Coding chalange 3 ##
 
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
 
+// if (scoreDolphins > scoreKoalas) {
+//     console.log('Dolphins win the trophy')
+// } else if (scoreDolphins < scoreKoalas) {
+//     console.log('Koalas win the trophy')
+// } else if (scoreDolphins === scoreKoalas) {
+//     console.log('Both win the trophy')
+// }
+
+// Bonus 1
+
+const scoreDolphins = (97 + 112 + 91) / 3;
+const scoreKoalas = (109 + 95 + 95) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && (scoreDolphins >= 100)) {
+    console.log('Dolphins win the trophy')
+} else if (scoreKoalas > scoreDolphins && (scoreKoalas >= 100)) {
+    console.log('Koalas win the trophy')
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log('Both win the trophy')
+} else {
+    console.log('No one wins the trophy')
+}
