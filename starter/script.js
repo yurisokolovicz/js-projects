974e6c6ca7ac0ce5081b106d70b4c5ebd2682ef8
yurisokolovicz <yurisokolovicz@gmail.com>
2023-01-17 09:39:54 -0300

    thlsn gay

--- starter/script.js
+++ starter/script.js
@@ -381,3 +381,49 @@ if (scoreDolphins > scoreKoalas && (scoreDolphins >= 100)) {
     console.log('No one wins the trophy')
 }
 */
+
+//## The Switch Statement - declaracao de troca##
+
+const day = 'friday'; //trocar o dia da semana
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan my course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend')
+        break;
+    default:
+        console.log('Not a valid day');
+}
+
+
+if (day === 'monday') {
+    console.log('Plan my course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend');
+} else {
+    console.log('Not a valid day');
+}
+
+
+//## Statements and Expressions ##
