4457f50bb98af962b9590360db25b1e556c42e91
yurisokolovicz <yurisokolovicz@gmail.com>
2023-01-17 15:47:09 -0300

    Reviewing Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -81,7 +81,7 @@ console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1998, 'Bob'));
 
 
- // ## 6. Functions Calling Other Functions ##
+// ## 6. Functions Calling Other Functions ##
 
 function cutFruitPieces(fruit) {
     return fruit * 11; 
@@ -97,4 +97,28 @@ function fruitProcessor(apples, oranges) {
 
 console.log(fruitProcessor(2, 3));
 
+
+// ## 7. Reviewing Functions ##
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function(birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+    } else {
+    console.log(`${firstName} has already retired`);
+    return -1;
+    }
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+
 */
+
