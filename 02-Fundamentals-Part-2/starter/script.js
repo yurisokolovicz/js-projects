d1c4f5e658247ee0d361952025266124402b4e74
yurisokolovicz <yurisokolovicz@gmail.com>
2023-01-17 12:00:47 -0300

    Functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,38 @@
+//## 2. Activating Strict Mode ##
+
+'use strict'; //Secure - Strict mode - cria erros visiveis no dev console
+/*
+let hasDriversLicense = false;
+const passTest = true; //por padrao devemos sempre usar const p evitar erros no codigo
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+// const interface ='Audio;'
+// const private = 534;
+*/
+
+//## 3. Functions ##
+
+function logger() { // () are parameters 
+    //function body
+    console.log('My name is Yuri');
+}
+//Parameters represent the input data of the funcion
+
+//Calling, running, invoking the function
+logger();//Nao chama nenhum parametro da funcao
+logger();//Apenas loga alguma coisa no console
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+    //const juice = String using the input data of the function
+}
+//Calling, running, invoking the function
+const appleJuice = fruitProcessor(5, 0); 
+console.log(appleJuice);
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+// 5, 0, 2, 4 are argumment
