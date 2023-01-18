fb408197d84ddea4161192dba2644b0d4f4a3815
yurisokolovicz <yurisokolovicz@gmail.com>
2023-01-18 20:20:23 -0300

    Handling Click Events partA

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent); //log to the console
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 
@@ -8,3 +8,8 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+});
