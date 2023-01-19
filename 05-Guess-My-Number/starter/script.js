936ee902cc3b2f48bbc174902d3d0c35b81a12cf
yurisokolovicz <yurisokolovicz@gmail.com>
2023-01-19 08:41:48 -0300

    Handling Click Events - part B

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,4 +12,8 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No number!';
+  }
 });
