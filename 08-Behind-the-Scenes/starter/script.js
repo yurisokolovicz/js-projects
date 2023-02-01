f0b162df830c76b26a1949b6d43659db574c5762
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-01 13:38:00 -0300

    Scoping in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,39 @@
-'use strict';
+"use strict";
+
+// Scoping in Practice
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = `Steven`;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      // Reassigning outer scope's variable
+      output = `NEW OUTPUT!`;
+    }
+    console.log(millenial);
+    // console.log(str);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = "Yuri";
+calcAge(1991);
+// console.log(age);
+// printAge();
