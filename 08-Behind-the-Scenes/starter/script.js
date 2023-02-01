f0c1febe588892f1e47dceeb168b79ebbdbd62f8
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-01 16:08:45 -0300

    The this Keyword

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// // Scoping in Practice
+// ## Scoping in Practice
 
 // function calcAge(birthYear) {
 //   const age = 2037 - birthYear;
@@ -38,47 +38,82 @@
 // // console.log(age);
 // // printAge();
 
-// Hoisting and TDZ in Practice
+// ## Hoisting and TDZ in Practice
 
-// Variables
-console.log(me);
-// console.log(job);
-// console.log(year);
+// // Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
 
-var me = "Yuri";
-let job = "Researcher";
-const year = 1991;
+// var me = "Yuri";
+// let job = "Researcher";
+// const year = 1991;
 
-// Functions
-console.log(addDecl(2, 3));
-// console.log(addExpr(2, 3));
-console.log(addArrow);
-// console.log(addArrow(2, 3));
+// // Functions
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// console.log(addArrow);
+// // console.log(addArrow(2, 3));
 
-function addDecl(a, b) {
-  return a + b;
-}
+// function addDecl(a, b) {
+//   return a + b;
+// }
 
-const addExpr = function (a, b) {
-  return a + b;
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+// // Example
+// console.log(undefined);
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log(`All products deleted!`);
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+// ## The this Keyword/Variable - will point to the object wich is calling the method - Dynamic and not static
+
+console.log(this); // Window Object
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1991);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
 };
 
-var addArrow = (a, b) => a + b;
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge(); // we are calling the method on matilda so this will point to matilda even this was inside jonas object
 
-// Example
-console.log(undefined);
-if (!numProducts) deleteShoppingCart();
-
-var numProducts = 10;
-
-function deleteShoppingCart() {
-  console.log(`All products deleted!`);
-}
-
-var x = 1;
-let y = 2;
-const z = 3;
-
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z);
+const f = jonas.calcAge;
+f(); // this keyweord is undefined
