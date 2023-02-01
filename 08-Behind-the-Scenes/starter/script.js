9e60b2717a690d7e80a2974a56f6e30f758f4d19
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-01 13:56:41 -0300

    Hoisting and TDZ in Practice part A

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,39 +1,52 @@
 "use strict";
 
-// Scoping in Practice
+// // Scoping in Practice
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      // Creating NEW variable with same name as outer scope's variable
-      const firstName = `Steven`;
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       // Creating NEW variable with same name as outer scope's variable
+//       const firstName = `Steven`;
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
+//       function add(a, b) {
+//         return a + b;
+//       }
 
-      // Reassigning outer scope's variable
-      output = `NEW OUTPUT!`;
-    }
-    console.log(millenial);
-    // console.log(str);
-    // console.log(add(2, 3));
-    console.log(output);
-  }
-  printAge();
+//       // Reassigning outer scope's variable
+//       output = `NEW OUTPUT!`;
+//     }
+//     console.log(millenial);
+//     // console.log(str);
+//     // console.log(add(2, 3));
+//     console.log(output);
+//   }
+//   printAge();
 
-  return age;
-}
+//   return age;
+// }
 
-const firstName = "Yuri";
-calcAge(1991);
-// console.log(age);
-// printAge();
+// const firstName = "Yuri";
+// calcAge(1991);
+// // console.log(age);
+// // printAge();
+
+// Hoisting and TDZ in Practice
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Yuri";
+let job = "Researcher";
+const year = 1991;
+
+// Functions
