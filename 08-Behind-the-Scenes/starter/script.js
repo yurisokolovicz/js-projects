75af44f762a4a684003a7bee9f7d16bdeb55fa8f
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-03 10:29:55 -0300

    Primitives vs. Objects (Primitive vs. Reference Types)

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -171,3 +171,24 @@
 // addExpr(2, 5, 8);
 
 // // Best Practice it is not to use arrow function as method.
+
+// ## Primitives vs. Objects (Primitive vs. Reference Types)
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age); // 31
+console.log(oldAge); // 30
+
+const me = {
+    name: 'Jonas',
+    age: 30
+};
+const friend = me;
+friend.age = 27;
+
+console.log('Friend', friend);
+// {name: 'Jonas', age: 27}
+
+console.log('Me', me);
+// {name: 'Jonas', age: 27}
