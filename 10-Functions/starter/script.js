089e712b7de47765cf7b8077d4e2f20f74c89d5f
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-12 15:26:59 -0300

    Higher-Order Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const flight = 'LH234';
 const yuri = {
   name: 'Yuri Sokolovicz',
@@ -31,3 +32,33 @@ const newPassport = function (person) {
 
 newPassport(yuri);
 checkIn(flight, yuri);
+*/
+
+// ## Higher-Order Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('Hi');
+};
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
