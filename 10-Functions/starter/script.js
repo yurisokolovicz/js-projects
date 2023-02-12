36cd2f052e22c0aa4d22cf379d6b1b21abd17eb3
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-12 18:14:41 -0300

    The bind Method

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -81,7 +81,7 @@ greet('Hello')('Yuri');
 const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
 greetArr('Hi')('Yuri');
-*/
+
 
 ///////////////////////////////////////
 // The call and apply Methods
@@ -133,4 +133,52 @@ console.log(swiss);
 
 book.call(swiss, ...flightData); // spread operator used in modern js
 
-// ## The bind Method
+///////////////////////////////////////
+// The bind Method - The most used
+// book.call(eurowings, 23, 'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Yuri Sokolovicz');
+bookEW23('Martha Cooper');
+
+// Bind with Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); // lufthansa object been the this keyword
+
+// Bind in Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// null is the this keyword
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+
+*/
