6a87ef45008dab60a6a06438dc20b6fcb84adc24
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-12 10:06:32 -0300

    The Spread Operator (...)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -31,25 +31,72 @@ const restaurant = {
         }
     },
 
-    oderDelivery: function ({ startedIndex = 1, mainIndex = 0, time = '20:00', address }) {
-        console.log(
-            `Order received! ${this.starterMenu[startedIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-        );
+    // oderDelivery: function ({ startedIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    //     console.log(
+    //         `Order received! ${this.starterMenu[startedIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    //     );
+    // }
+    orderPasta: function (ing1, ing2, ing3) {
+        console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
     }
 };
 
-restaurant.oderDelivery({
-    time: '22:30',
-    address: 'Via del Sole, 21',
-    mainIndex: 2,
-    starterIndex: 2
-});
+// restaurant.oderDelivery({
+//     time: '22:30',
+//     address: 'Via del Sole, 21',
+//     mainIndex: 2,
+//     starterIndex: 2
+// });
 
-restaurant.oderDelivery({
-    address: 'Via del Sole, 21',
-    startedIndex: 1
-});
+// restaurant.oderDelivery({
+//     address: 'Via del Sole, 21',
+//     startedIndex: 1
+// });
 
+// ## The Spread Operator (...)
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+
+// Insert new data in the array
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.'];
+console.log(letters);
+
+// Real-world examples
+// const ingredients = [prompt("Let's make pasta! Ingredient 1?"), prompt('Ingredient 2?'), prompt('Ingredient 3?')];
+
+// console.log(ingredients);
+
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Giuseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+/*
 // Destructuring Objects
 
 const { name, openingHours, categories } = restaurant;
@@ -75,7 +122,7 @@ const {
 } = openingHours;
 console.log(o, c);
 
-/*
+
 ///////////////////////////////////////////////////////
 // Destructuring Arrays
 
