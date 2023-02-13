eb74d490e182fd9dd9ba19d932d44ebdfcdabda3
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-13 10:14:47 -0300

    The new at Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -111,3 +111,17 @@ console.log(letters.join(' - '));
 console.log(letters.join(' _ '));
 console.log(letters.join(' + '));
 */
+
+///////////////////////////////////////
+// The new at Method - New ES2022
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1)); // Easier code
+
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
