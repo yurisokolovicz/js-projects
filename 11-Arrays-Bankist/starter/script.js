5c454173046c8a1aba4a7ad549259484ef1ec888
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-13 10:36:20 -0300

    Looping Arrays: forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
     ['GBP', 'Pound sterling']
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /*
 /////////////////////////////////////////////////
@@ -110,7 +110,7 @@ console.log([...arr, ...arr2]); // Spread operator method
 console.log(letters.join(' - '));
 console.log(letters.join(' _ '));
 console.log(letters.join(' + '));
-*/
+
 
 ///////////////////////////////////////
 // The new at Method - New ES2022
@@ -125,3 +125,31 @@ console.log(arr.at(-1)); // Easier code
 
 console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
+
+*/
+
+///////////////////////////////////////
+// Looping Arrays: forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) { }
+for (const [i, movement] of movements.entries()) {
+    if (movement > 0) {
+        console.log(`Movement ${i + 1}: You deposited ${movement}`);
+    } else {
+        console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+    }
+}
+
+console.log('---- FOREACH ----'); // Easier method
+movements.forEach(function (mov, i, arr) {
+    if (mov > 0) {
+        console.log(`Movement ${i + 1}: You deposited ${mov}`);
+    } else {
+        console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+    }
+});
+// 0: function(200) - Behind the scenes
+// 1: function(450)
+// 2: function(400)
+// ...
