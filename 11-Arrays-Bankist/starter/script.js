c55b85b372499704c61fe4d7de605fb32c7db428
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-14 11:05:46 -0300

    Implement map, filter and reduce Method together

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const displayMovements = function (movements) {
         const html = `
           <div class="movements__row">
             <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-            <div class="movements__value">${mov}</div>
+            <div class="movements__value">${mov}€</div>
           </div>  
     `;
         containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -81,9 +81,28 @@ displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
     const balance = movements.reduce((acc, mov) => acc + mov, 0);
-    labelBalance.textContent = `${balance} EUR`;
+    labelBalance.textContent = `${balance}€`;
 };
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySumary = function (movements) {
+    const incomes = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
+    labelSumIn.textContent = `${incomes}€`;
+
+    const outcomes = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
+    labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+
+    const interest = movements
+        .filter(mov => mov > 0)
+        .map(deposit => (deposit * 1.2) / 100)
+        .filter((int, i, arr) => {
+            // console.log(arr);
+            return int >= 1;
+        })
+        .reduce((acc, int) => acc + int, 0);
+    labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySumary(account1.movements);
 
 const createUserNames = function (accs) {
     accs.forEach(function (acc) {
@@ -247,7 +266,6 @@ console.log(depositsFor);
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
 
-*/
 
 ///////////////////////////////////////
 // The reduce Method
@@ -272,3 +290,21 @@ const max = movements.reduce((acc, mov) => {
     else return mov;
 }, movements[0]); // First element of the array
 console.log(max);
+
+*/
+
+///////////////////////////////////////
+// The Magic of Chaining Methods
+const eurToUsd = 1.1;
+console.log(movements);
+
+// PIPELINE
+const totalDepositsUSD = movements
+    .filter(mov => mov > 0)
+    .map((mov, i, arr) => {
+        // console.log(arr);
+        return mov * eurToUsd;
+    })
+    // .map(mov => mov * eurToUsd)
+    .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
