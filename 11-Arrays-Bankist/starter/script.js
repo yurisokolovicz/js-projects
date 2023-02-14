f803b1d6340d15f40d8066cab1a74b8472aa3c13
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-14 10:22:16 -0300

    The reduce Method to make the balance account

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -79,6 +79,12 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+    const balance = movements.reduce((acc, mov) => acc + mov, 0);
+    labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
 const createUserNames = function (accs) {
     accs.forEach(function (acc) {
         acc.username = acc.owner
@@ -225,7 +231,7 @@ console.log(movementsUSDfor);
 const movementsDescriptions = movements.map((mov, i) => `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`);
 console.log(movementsDescriptions);
 
-*/
+
 ///////////////////////////////////////
 // The filter Method
 const deposits = movements.filter(function (mov, i, arr) {
@@ -240,3 +246,29 @@ console.log(depositsFor);
 
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
+
+*/
+
+///////////////////////////////////////
+// The reduce Method
+console.log(movements);
+
+// acc = accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+// 0 is the initial interal value of the accumulator in the first loop iteration
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0; // Need external value = unpractical when we use many loops for doing many operations
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+    if (acc > mov) return acc;
+    else return mov;
+}, movements[0]); // First element of the array
+console.log(max);
