81629bfea65051e7d7dc457e02ccd9dcd43ee5ec
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-16 15:32:05 -0300

    Operations With Dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -16,14 +16,14 @@ const account1 = {
     pin: 1111,
 
     movementsDates: [
-        '2019-11-18T21:31:17.178Z',
-        '2019-12-23T07:42:02.383Z',
-        '2020-01-28T09:15:04.904Z',
-        '2020-04-01T10:17:24.185Z',
-        '2020-05-08T14:11:59.604Z',
-        '2020-05-27T17:01:17.194Z',
-        '2020-07-11T23:36:17.929Z',
-        '2020-07-12T10:51:36.790Z'
+        '2022-08-18T21:31:17.178Z',
+        '2022-09-23T07:42:02.383Z',
+        '2022-10-28T09:15:04.904Z',
+        '2022-11-01T10:17:24.185Z',
+        '2023-01-08T14:11:59.604Z',
+        '2023-02-06T17:01:17.194Z',
+        '2023-02-12T23:36:17.929Z',
+        '2023-02-15T10:51:36.790Z'
     ],
     currency: 'EUR',
     locale: 'pt-PT' // de-DE
@@ -36,14 +36,14 @@ const account2 = {
     pin: 2222,
 
     movementsDates: [
-        '2019-11-01T13:15:33.035Z',
-        '2019-11-30T09:48:16.867Z',
-        '2019-12-25T06:04:23.907Z',
-        '2020-01-25T14:18:46.235Z',
-        '2020-02-05T16:33:06.386Z',
-        '2020-04-10T14:43:26.374Z',
-        '2020-06-25T18:49:59.371Z',
-        '2020-07-26T12:01:20.894Z'
+        '2021-11-01T13:15:33.035Z',
+        '2021-11-30T09:48:16.867Z',
+        '2021-12-25T06:04:23.907Z',
+        '2022-01-25T14:18:46.235Z',
+        '2022-02-05T16:33:06.386Z',
+        '2022-04-10T14:43:26.374Z',
+        '2023-02-05T18:49:59.371Z',
+        '2023-02-10T12:01:20.894Z'
     ],
     currency: 'USD',
     locale: 'en-US'
@@ -480,3 +480,16 @@ future.setFullYear(2040);
 console.log(future);
 
 */
+
+///////////////////////////////////////
+// Operations With Dates
+const future = new Date(2037, 10, 19, 15, 23); // result in milliseconds
+console.log(+future);
+
+// / (1000 * 60 * 60 * 24) is to convert milliseconds in days
+// Math.abs eh para nao ficar diferenca de dia negativo
+const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+// To calculate the diference in days between two dates
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 10));
+console.log(days1);
