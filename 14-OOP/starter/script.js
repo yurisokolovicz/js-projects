3a801516d0b8d8c60f58d1fe6c6f65502b899f6e
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-27 11:37:33 -0300

    Encapsulation: Private Class Fields and Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -499,7 +499,7 @@ jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
 
-*/
+
 
 ///////////////////////////////////////
 // Another Class Example
@@ -541,7 +541,7 @@ class Account {
   }
 }
 
-const acc1 = new Account('Yuri', 'EUR', 1111);
+const acc1 = new Account('Yuri', 'BR', 1111);
 
 // Deposit and Withdrawn
 
@@ -556,3 +556,90 @@ console.log(acc1.getMovements());
 console.log(acc1);
 // This is why we need data encapsulation to secure pin and important stuffs
 // console.log(acc1._pin);
+
+*/
+
+///////////////////////////////////////
+// Encapsulation: Protected Properties and Methods
+// Encapsulation: Private Class Fields and Methods
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also the static version)
+
+class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields (instances) - Use # before the name
+  #movements = [];
+  #pin; // defined outside of any method
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+
+    // Protected property
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // 3) Public methods
+
+  // Public interface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  requestLoan(val) {
+    // if (this.#approveLoan(val)) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+      return this;
+    }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account('Yuri', 'BR', 1111);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+Account.helper();
+
+//// DOES NOT WORK, IT IS NOW PROTECTED - FULLY PRIVATE (inaccessible from outside)
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100)); // Browser does not support yet
+
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
