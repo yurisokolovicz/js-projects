ee290fdd6a4e3c0cde2a2dd1aa137df55629a441
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-27 16:55:22 -0300

    Coding Challenge #4

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -557,7 +557,7 @@ console.log(acc1);
 // This is why we need data encapsulation to secure pin and important stuffs
 // console.log(acc1._pin);
 
-*/
+
 
 ///////////////////////////////////////
 // Encapsulation: Protected Properties and Methods
@@ -644,3 +644,77 @@ Account.helper();
 // Need to set "return this;" in the deposit, withdraw, requestLoan, etc...
 acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1.getMovements());
+
+
+
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+
+*/
+
+class CarCl {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+        return this;
+    }
+
+    get speedUS() {
+        return this.speed / 1.6;
+    }
+
+    set speedUS(speed) {
+        this.speed = speed * 1.6;
+    }
+}
+
+// Create an 'EVCl' child class of the 'CarCl' class
+class EVCl extends CarCl {
+    #charge;
+    constructor(make, speed, charge) {
+        super(make, speed);
+        this.#charge = charge;
+    }
+
+    chargeBattery(chargeTo) {
+        this.#charge = chargeTo;
+        return this;
+    }
+
+    accelerate() {
+        this.speed += 20;
+        this.#charge--;
+        console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.#charge}`);
+        return this;
+    }
+}
+
+// Creat an object 'Rivian' going at 120 km/h, with a charge of 23%
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+// console.log(rivian.#charge); // Totally privated, Encapsulated inside the class
+
+// Chain
+rivian.accelerate().accelerate().accelerate().brake().chargeBattery(100).accelerate().accelerate();
+
+console.log(rivian.speedUS);
