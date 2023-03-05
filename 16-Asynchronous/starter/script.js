33b4f734b350d8e2168261e8095304681d9a2950
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-05 16:04:38 -0300

    Chaining Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -116,7 +116,7 @@ setTimeout(() => {
 }, 1000);
 
 
-*/
+
 
 //////////////////////////////////
 // Promises and the Fetch API
@@ -167,3 +167,50 @@ const getCountryData = function (country) {
 };
 
 getCountryData('portugal');
+
+*/
+//////////////////////////////////
+// Chaining Promises
+
+const renderCountry = function (data, className = '') {
+    const languages = data.languages[Object.keys(data.languages)[0]];
+
+    const currencies = data.currencies[Object.keys(data.currencies)[0]].name;
+
+    const html = `
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}m people</p>
+            <p class="country__row"><span>🗣️</span>${languages}</p>
+            <p class="country__row"><span>💰</span>${currencies}</p>
+          </div>
+    </article>
+    `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+    // Country 1
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+        .then(response => response.json())
+        .then(data => {
+            renderCountry(data[0]);
+            console.log(data);
+            const neighbour = data[0].borders[0];
+            console.log(neighbour);
+
+            if (!neighbour) return;
+
+            // Country 2
+            return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+        })
+        .then(response => response.json())
+        .then(data => renderCountry(data[0], 'neighbour'));
+};
+
+getCountryData('brazil');
