1e546e5e8cf76b614d068590468ed510d9b42645
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-05 11:29:48 -0300

    Callback Hell

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -5,7 +5,7 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 // Our First AJAX Call: XMLHttpRequest
-
+/*
 const getCountryData = function (country) {
     const request = new XMLHttpRequest();
     request.open('GET', `https://restcountries.com/v3.1/name/${country}?fullText=true`);
@@ -40,3 +40,77 @@ const getCountryData = function (country) {
 getCountryData('brazil');
 getCountryData('portugal');
 getCountryData('germany');
+*/
+/////////////////////////////////////////
+// Welcome to Callback Hell
+
+const renderCountry = function (data, className = '') {
+    const languages = data.languages[Object.keys(data.languages)[0]];
+
+    const currencies = data.currencies[Object.keys(data.currencies)[0]].name;
+
+    const html = `
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}m people</p>
+            <p class="country__row"><span>🗣️</span>${languages}</p>
+            <p class="country__row"><span>💰</span>${currencies}</p>
+          </div>
+    </article>
+    `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+    // AJAX call country 1
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}?fullText=false`);
+    request.send();
+
+    request.addEventListener('load', function () {
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+
+        // Render country 1
+        renderCountry(data);
+
+        // Get neighbour country 1 (country 2)
+        const [neighbour] = data.borders; // borders?.[0]
+
+        if (!neighbour) return;
+
+        // AJAX call country 2
+        const request2 = new XMLHttpRequest();
+        request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+        request2.send();
+
+        request2.addEventListener('load', function () {
+            const [data2] = JSON.parse(this.responseText);
+            console.log(data2);
+
+            renderCountry(data2, 'neighbour');
+        });
+    });
+};
+
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+
+// Callback Hell - a lot of nested functions
+setTimeout(() => {
+    console.log('1 second passed');
+    setTimeout(() => {
+        console.log('2 seconds passed');
+        setTimeout(() => {
+            console.log('3 seconds passed');
+            setTimeout(() => {
+                console.log('4 second passed');
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
