d439f2299ad2f4cd5c0c00086bef209a9f0c90c2
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-06 15:23:06 -0300

    Promisifying the Geolocation API

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -330,7 +330,8 @@ Promise.reject(new Error('Problem!')).catch(x => console.error(x));
 
 */
 
-///////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+//##################### Geolocation API #########################
 // Coding Challenge #1
 
 /* 
@@ -355,7 +356,7 @@ TEST COORDINATES 2: 19.037, 72.873
 TEST COORDINATES 2: -33.933, 18.474
 
 GOOD LUCK 😀
-*/
+
 
 // Old needed data for the challenge
 const renderCountry = function (data, className = '') {
@@ -408,3 +409,72 @@ whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
 
 // 'https://geocode.xyz/51.50354,-0.12768?geoit=xml&auth=your_api_key'
+
+*/
+
+/////////////////////////////////////////////////////////////////
+//############# Promisifying theGeolocation API ################
+
+const renderCountry = function (data, className = '') {
+    const languages = data.languages[Object.keys(data.languages)[0]];
+
+    const currencies = data.currencies[Object.keys(data.currencies)[0]].name;
+
+    const html = `
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}m people</p>
+            <p class="country__row"><span>🗣️</span>${languages}</p>
+            <p class="country__row"><span>💰</span>${currencies}</p>
+          </div>
+    </article>
+    `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
+
+const getPosition = function () {
+    return new Promise(function (resolve, reject) {
+        // navigator.geolocation.getCurrentPosition(
+        //     position => resolve(position),
+        //     err => reject(err)
+        // );
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+};
+
+getPosition().then(position => console.log(position));
+
+// Geolocation API (Coding Challenge #1)
+const whereAmI = function () {
+    getPosition()
+        .then(position => {
+            const { latitude: lat, longitude: lng } = position.coords;
+
+            return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json&auth=148242640815718264297x10968`);
+        })
+        .then(res => {
+            if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+            return res.json();
+        })
+        .then(data => {
+            // console.log(data);
+            console.log(`You are in ${data.city}, State of ${data.state}, ${data.country}. Timezone: ${data.timezone}`);
+
+            return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+        })
+        .then(res => {
+            if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+            return res.json();
+        })
+        .then(data => renderCountry(data[0]))
+        .catch(err => console.error(`${err.message} 💥`));
+};
+
+btn.addEventListener('click', whereAmI);
+// whereAmI();
