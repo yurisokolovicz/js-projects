dc00086571a42b28fcdb98b245b85099dc586114
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-07 10:54:13 -0300

    Returning Values from Async Functions

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -629,7 +629,7 @@ const whereAmI = async function () {
 whereAmI();
 console.log('FIRST');
 
-*/
+
 
 /////////////////////////////////////////////////////////////////
 //##################### try...catch #########################
@@ -703,3 +703,87 @@ console.log('FIRST');
 // } catch (err) {
 //     alert(err.message);
 // }
+
+*/
+
+/////////////////////////////////////////////////////////////////
+//############# Returning Values from Async Functions #############
+
+const renderCountry = function (data, className = '') {
+    const languages = data.languages[Object.keys(data.languages)[0]];
+
+    const currencies = data.currencies[Object.keys(data.currencies)[0]].name;
+
+    const html = `
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}m people</p>
+            <p class="country__row"><span>🗣️</span>${languages}</p>
+            <p class="country__row"><span>💰</span>${currencies}</p>
+          </div>
+    </article>
+    `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
+
+const getPosition = function () {
+    return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+};
+
+const whereAmI = async function () {
+    try {
+        // Geolocation
+        const position = await getPosition();
+        const { latitude: lat, longitude: lng } = position.coords;
+
+        // Reverse geocoding
+        const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json&auth=148242640815718264297x10968`);
+        if (!resGeo.ok) throw new Error('Problem getting location data');
+
+        const dataGeo = await resGeo.json();
+
+        // Country data
+        const res = await fetch(`https://restcountries.com/v3.1/name/${dataGeo.country}`);
+        if (!res.ok) throw new Error('Problem getting country');
+        const data = await res.json();
+        console.log(data);
+        renderCountry(data[0]);
+
+        return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+    } catch (err) {
+        console.error(`${err} 💥`);
+        renderError(`💥 ${err.message}`);
+
+        // Reject promise returned from async function
+        throw err;
+    }
+};
+
+console.log('1: Will get location');
+// const city = whereAmI();
+// console.log(city);
+
+// Returning Values from Async Functions mixed with then() method - mixture is very bad
+// whereAmI()
+//     .then(city => console.log(`2: ${city}`))
+//     .catch(err => console.error(`2: ${err.message} 💥`))
+//     .finally(() => console.log('3: Finished getting location'));
+
+// Returning Values from Async Functions
+// IIFEs - Immediately-invoked functions expressions
+(async function () {
+    try {
+        const city = await whereAmI();
+        console.log(`2: ${city}`);
+    } catch (err) {
+        console.error(`2: ${err.message} 💥`);
+    }
+    console.log('3: Finished getting location');
+})();
