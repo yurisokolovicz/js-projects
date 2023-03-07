1eb95b961c74b1958ff6a40825baff634da36a9c
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-07 12:07:42 -0300

    Running Promises in Parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -704,7 +704,7 @@ console.log('FIRST');
 //     alert(err.message);
 // }
 
-*/
+
 
 /////////////////////////////////////////////////////////////////
 //############# Returning Values from Async Functions #############
@@ -787,3 +787,71 @@ console.log('1: Will get location');
     }
     console.log('3: Finished getting location');
 })();
+
+*/
+
+/////////////////////////////////////////////////////////////////
+//################ Running Promises in Parallel ################
+
+// ASYNC function always need to use try catch block
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+    return fetch(url).then(response => {
+        if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+        return response.json();
+    });
+};
+
+const get3Countries = async function (c1, c2, c3) {
+    try {
+        // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+        // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+        // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+        // console.log([data1.capital[0], data2.capital[0], data3.capital[0]]);
+
+        // Promise all combinator - run all the promises in the array at the same time
+        const data = await Promise.all([
+            getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+            getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+            getJSON(`https://restcountries.com/v3.1/name/${c3}`)
+        ]);
+        console.log(data);
+        console.log(data.map(d => d[0].capital[0]));
+    } catch (err) {
+        console.error(err);
+        // renderError(`💥 ${err.message}`);
+    }
+};
+get3Countries('brazil', 'canada', 'usa');
+
+// https://restcountries.com/v3.1/name/${c1}
+
+// // Just for the sake of practicing I rewrote the original getJSON() function to use async/await. And then I didn't want to repeat the URL in each call, so I defined the API url once and then passed that in as parameter with the country to getJSON() and completed the url there.
+
+// const getJSON = async function (api, country) {
+//     try {
+//         const url = `${api}${country}`;
+//         const res = await fetch(url);
+//         const data = await res.json();
+//         return data;
+//     } catch (err) {
+//         console.error(`${err} 💥`);
+//         renderError(`💥 ${err.message}`);
+//     }
+// };
+
+// const get3Countries = async function (c1, c2, c3) {
+//     try {
+//         const api = `https://restcountries.com/v3.1/name/`;
+//         // Promise all combinator - run all the promises in the array at the same time
+//         const data = await Promise.all([getJSON(api, c1), getJSON(api, c2), getJSON(api, c3)]);
+//         // console.log(data); // for checking
+//         console.log(data.map(d => d[0].capital[0]));
+//     } catch (err) {
+//         console.error(`${err} 💥`);
+//         renderError(`💥 ${err.message}`);
+//     }
+// };
+
+// get3Countries('brazil', 'usa', 'italy');
