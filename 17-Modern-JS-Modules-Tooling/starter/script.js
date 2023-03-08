350f97bf1b819533f4793ed5c15164e975e607cb
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-08 10:02:19 -0300

    Top-Level await (ES2022)

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -37,3 +37,38 @@ add('apples', 4);
 // console.log(cart);
 
 ///// Imports are not copies of the export, it point to the same place in memory
+
+///////////////////////////////////////
+// Top-Level Await (ES2022)
+
+// From the ES2022 we can now use the await keyword outside of async functions, at least in modules (script type="module")
+
+// Problem: the await now is blocking the entire execution of this module because there is not more async function
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json(); // To parse data as json
+// console.log(data);
+// console.log('Something');
+
+// Situation in which we need to return data from async function
+const getLastPost = async function () {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+    console.log(data);
+    // We want to get the last element of the array [99] using the new ES2022 'at method'
+    return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// Why it is called promise? because it is async function, it does not comes immediately, it is a promise that it will return soon
+// getLastPost();
+
+// DOES NOT RETURN THE DATA BUT JUST A PROMISSE, NEED TO USE TOP LVL AWAIT
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// HERE THE TOP LVL AWAIT IS VERY USEFULL
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,6 +1,14 @@
 // Exporting module
 console.log('Exporting module');
 
+// Blocking code
+// Using top-level await (await outside of async function) will block the entire module
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
+
+//////////////////////
+
 const shippingCost = 10;
 export const cart = [];
 
