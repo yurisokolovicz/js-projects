4d92d2998da79884fccd1308a6b3dc8e79fe72bc
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-08 12:57:21 -0300

    Introduction to NPM

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -132,3 +132,20 @@ export.addTocart = function (product, quantity) {
 const { addTocart } = require('./shoppingCart.js');
 
 */
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+    cart: [
+        { product: 'bread', quantity: 5 },
+        { product: 'pizza', quantity: 5 }
+    ],
+    user: { loggedIn: true }
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+
+console.log(stateClone);
+
+console.log(stateDeepClone);
