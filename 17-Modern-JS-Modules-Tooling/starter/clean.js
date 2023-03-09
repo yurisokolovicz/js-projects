0b8894db09fa3f321fb679d61e131e48d6e29104
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-09 11:18:08 -0300

    Fix Bad Code: Part 2

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,7 @@
-const budget = [
+'strict mode';
+
+// Object.freeze just freeze the first level of the objects, it is not deep freeze, we can still changing the objects inside the objects
+const budget = Object.freeze([
     { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
     { value: -45, description: 'Groceries 🥑', user: 'jonas' },
     { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,41 +10,81 @@ const budget = [
     { value: -20, description: 'Candy 🍭', user: 'matilda' },
     { value: -125, description: 'Toys 🚂', user: 'matilda' },
     { value: -1800, description: 'New Laptop 💻', user: 'jonas' }
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
     jonas: 1500,
     matilda: 100
+});
+/////// 1st level of the object is freezed
+// spendingLimits.jay = 200;
+// console.log(spendingLimits);
+// budget[9] = 'jonas';
+
+////// 2nd lvl of the object is not freezed
+budget[0].value = 300;
+
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+//// Impure function because it does attempt to manipulate and to mutate the budget object that is located outside of it.
+// const addExpense = function (value, description, user = 'jonas') {
+//     user = user.toLowerCase();
+
+//     if (value <= getLimit(user)) {
+//         budget.push({ value: -value, description, user });
+//     }
+// };
+// addExpense(10, 'Pizza 🍕');
+// addExpense(100, 'Going to movies 🍿', 'Matilda');
+// addExpense(200, 'Stuff', 'Jay');
+
+//// Pure function - not produce side effect- Important also in React.js
+const addExpense = function (state, limits, value, description, user = 'jonas') {
+    const cleanUser = user.toLowerCase();
+
+    return value <= getLimit(limits, cleanUser) ? [...state, { value: -value, description, cleanUser }] : state;
 };
 
-const getLimit = user => (spendingLimits[user] ? spendingLimits[user] : 0);
+//// Immutability - Important also in React.js
+// Calling the addExpense function will not mutate the budget object
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(budget, spendingLimits, 100, 'Going to movies 🍿', 'Matilda');
+const newBudget3 = addExpense(budget, spendingLimits, 200, 'Stuff', 'Jay');
 
-const addExpense = function (value, description, user = 'jonas') {
-    user = user.toLowerCase();
+//// Impure function
+// const checkExpenses = function () {
+//     for (const entry of newBudget3) if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
+// };
+// checkExpenses();
 
-    // const limit = getLimit(user);
-    // const limit = spendingLimits?.[user] ?? 0; // ES2022
+//// Pure function - does not mutate anything because the map return a new array
+// const checkExpenses = function (state, limits) {
+//     return state.map(entry => {
+//         return entry.value < -getLimit(limits, entry.user) ? { ...entry, flag: 'limit' } : entry;
+//     });
+// };
 
-    if (value <= getLimit(user)) {
-        budget.push({ value: -value, description, user });
-    }
+const checkExpenses = (state, limits) =>
+    state.map(entry => (entry.value < -getLimit(limits, entry.user) ? { ...entry, flag: 'limit' } : entry));
+
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget); // Impure
+
+//// Immutability of regular variables - in functional codes we never use let because it mutates de variable
+const logBigExpenses = function (state, bigLimit) {
+    //// Declarative code
+    const bigExpenses = state
+        .filter(entry => entry.value <= -bigLimit)
+        .map(entry => entry.description.slice(-2))
+        .join(' / ');
+
+    console.log(bigExpenses); // Impure
+
+    //// Imperative code
+    // let output = '';
+    // for (const entry of budget) output += entry.value <= -bigLimit ? `${entry.description.slice(-2)} /` : '';
+    // output = output.slice(0, -2); // Remove last '/ '
+    // console.log(output);
 };
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
 
-const checkExpenses = function () {
-    for (const entry of budget) if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
-};
-checkExpenses();
-
-const logBigExpenses = function (bigLimit) {
-    let output = '';
-    for (const entry of budget) output += entry.value <= -bigLimit ? `${entry.description.slice(-2)} /` : '';
-
-    output = output.slice(0, -2); // Remove last '/ '
-    console.log(output);
-};
-
-console.log(budget);
-logBigExpenses(1000);
+logBigExpenses(finalBudget, 500);
